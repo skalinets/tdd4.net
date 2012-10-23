b3e6d80c2bcf9afa58bf28ac72880774f4cab15f
skalinets <kalinets@gmail.com>
2012-10-23 21:40:53 +0300

    added support for file content

--- Tdd4.net/src/Tdd4Net.Tests/BlogTests.cs
+++ Tdd4.net/src/Tdd4Net.Tests/BlogTests.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using FizzWare.NBuilder;
 using FluentAssertions;
+using HtmlAgilityPack;
 using NSubstitute;
 using Tdd4.net.Business;
 using Xunit;
@@ -59,7 +62,10 @@ namespace Tdd4Net.Tests
 
         public GithubProviderTester()
         {
-            githubProvider = new GithubProvider("https://raw.github.com/skalinets/tdd4net.posts/for-tests/");
+            var userName = "skalinets";
+            var branch = "for-tests";
+            var repoName = "tdd4net.posts";
+            githubProvider = new GithubProvider(userName, repoName, branch);
         }
 
         [Fact]
@@ -68,22 +74,66 @@ namespace Tdd4Net.Tests
             var result = githubProvider.GetText("posts/test-post.mb");
             result.Should().Be("this is test post");
         }
+
+        [Fact]
+        public void should_get_folder_content()
+        {
+            githubProvider.GetFiles("posts")
+                .Should()
+                .BeEquivalentTo(new[] {"test-post", "another-post"});
+        }
     }
 
     public class GithubProvider
     {
-        private readonly string root;
+        private readonly string userName;
+        private readonly string repoName;
+        private readonly string branch;
 
-        public GithubProvider(string root)
+        public GithubProvider(string userName, string repoName, string branch)
         {
-            this.root = root;
+            this.userName = userName;
+            this.repoName = repoName;
+            this.branch = branch;
+
         }
 
         public string GetText(string postId)
         {
-            var webClient = new WebClient();
-            var address = root + postId;
-            return webClient.DownloadString(new Uri(address)); 
+            return DownloadString(GetFileRoot() + postId);
+        }
+
+        private object GetFileRoot()
+        {
+            return string.Format("https://raw.github.com/{0}/{1}/{2}/", userName, repoName, branch);
+        }
+
+        private string DownloadString(string newAddress)
+        {
+            using (var webClient = new WebClient())
+            {
+                return webClient.DownloadString(new Uri(newAddress));
+            }
+        }
+
+        public IEnumerable<string> GetFiles(string path)
+        {
+            var directoryAddress = GetDirectoryRoot() + path;
+            var filesPageContent = DownloadString(directoryAddress);
+            var doc = new HtmlDocument();
+            doc.LoadHtml(filesPageContent);
+            var files = doc.DocumentNode
+                .SelectNodes("//td[@class='content']/a")
+                .SelectMany(node => node.Attributes.Where(a => a.Name == "href")
+                    .Select(a => a.Value))
+                    .Select(link => Regex.Match(link, @"/(?<filename>(\w|-|_|\s)+)\.mb$").Groups["filename"].Value)
+                    .ToList();
+            return files;
+        }
+
+        private  object GetDirectoryRoot()
+        {
+            return string.Format("https://github.com/{0}/{1}/tree/{2}/", userName, repoName, branch);
         }
     }
 }
